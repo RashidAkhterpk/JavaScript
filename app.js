db6f20be1f6c3dbb7ff925a018b26ca6d26e23b1
RashidAkhterpk <rashidakhter2026@gmail.com>
2025-09-04 16:51:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,414 @@
+// // Chapter No. 1  --- ALERTS
+// // -------------------------
+
+// // Q-1
+// alert("Welcome to my Website");
+
+// // Q-2
+// alert("Error! Please enter a valid password");
+
+// // Q-3
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+// // Q-4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// // Q-5
+// console.log("Hello... I can run JS through my web browser's console");
+
+// // Q-6 & 7
+
+// // Chapter No. 2  --- VARIABLES FOR STRINGS 
+// // ----------------------------------------
+
+// // Q-1
+// var username;
+
+// // Q-2
+
+// var username = "Rashid Akhter";
+
+// // Q-3
+//     // a) 
+//     var message;
+
+//     // b)
+//     message = "Hello World";
+
+//     // c)
+//     alert(message)
+
+// // Q-4
+// var stdNm = "Jhone Doe"; 
+// var stdAge = 15;
+// var stdCrs = "Certified Mobile Application Development";
+// alert(stdNm);
+// alert(stdAge);
+// alert(stdCrs);
+
+// // Q-5
+
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// // Q-6
+
+// var email = "rashidakhter08051972@gmail.com";
+
+// alert("My email address is "+email);
+
+// // Q-7
+
+// var Book = "A smarter way to learn JavaScript";
+// alert("I m trying to learn from the Book "+Book);
+
+// // Q-8
+// document.write("Yah! I can write HTML content through JavaScript"+"<br>");
+
+// // Q-9
+// var abc = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"+"<br>";
+// alert(abc);
+// document.write(abc);
+
+
+// // Chapter No. 3  --- VARIABLES FOR NUMBERS 
+// // ----------------------------------------
+
+// // Q-1
+
+// var age = 15;
+// alert("I am " + age + " years old")
+
+// // Q-2
+// var N = 14
+// alert("You have visited this site " + N + " times");
+
+// // Q-3
+
+// var birthyear = "1990"
+// // alert("My birth year is "+birthyear+"\n Data type of my delcared variable is Number");
+// document.write("My birth year is " + birthyear + "\n Data type of my delcared variable is Number");
+// // document.write("Data type of my delcared variable is Number");
+
+// Q - 4
+
+// var name = prompt("Enter Viositor's Name");
+// var title = prompt("Enter Product Title");
+// var qty = prompt("Enter Quantity");
+
+// document.write(name + " " + "ordered" + qty + " " + title + " " + "From XYZ Clothing Store");
+
+// // Chapter No. 4  --- VARIABLE NAMES: LEGAL & ILLEGAL
+// // --------------------------------------------------
+
+// // Q-1
+// var x, y, z;
+
+// // Q-2
+// var numbers, $, _, _d2, e2, 1a, 2b, 3c, @d, &e
+
+// // Q-3
+//     // a)
+//     document.write("Rules for naming JS variables");
+
+//     // b)
+//     var numbers, $, _, _d2, e2, 1a, 2b, 3c, @d, &e, $my1stVariable
+//     document.write("Variable names can only contain "+numbers,$+"and"+" "+_+"."+"For Example:"+" ",$my1stVariable+"<br>");
+//     alert("Variable names can only contain "+numbers,$+"and"+" "+_+"."+"For Example:"+" ",$my1stVariable+"<br>");
+
+//     // c)
+//     document.writeln("Variables must begin with a ______, ______ or _____. For example $name, _name or name"+"<br>");
+    
+//     // d)
+
+//     document.write("Variable names are case sensitive"+"<br>");
+
+//     // e)
+//     document.write("Variable names should not be JS keywords"+"<br>")
+
+//     // Chapter No. 5  --- MATH EXPRESSIONS
+//     // -----------------------------------
+
+//     // Q-1
+//     var val1 = +prompt("1st Value");
+//     var val2 = +prompt("2nd Value");
+//     var val3 = val1 + val2;
+
+//     document.write("Sum of "+val1+" and "+val2+" is "+val3);
+
+//     // Q-2
+//     var val1 = +prompt("1st Value");
+//     var val2 = +prompt("2nd Value");
+//     var val3 = val1 + val2;
+//     var val4 = val1 - val2;
+//     var val5 = val1 * val2;
+//     var val6 = val1 / val2;
+//     var val7 = val1 % val2;
+
+//     document.write("Addition of "+val1+" and "+val2+" is "+val3+"<br>");
+
+//     document.write("Substraction of "+val1+" and "+val2+" is "+val4+"<br>");
+
+//     document.write("Multiplication of "+val1+" and "+val2+" is "+val5+"<br>");
+
+//     document.write("Division of "+val1+" and "+val2+" is "+val6+"<br>");
+
+//     document.write("Modulus of "+val1+" and "+val2+" is "+val7);
+
+//     // Q-3
+//         // a)
+//         var myVal;
+
+//         // b)
+//         document.write("Value after variable declaration is: "+myVal+"<br>");
+
+//         // c)
+//         myVal = 5
+
+//         // d)
+//         document.write("Initial value:"+myVal+"<br>")
+
+//         // e)
+//         myVal++
+
+//         // f)
+//         document.write("Value after increment is:"+myVal+"<br>")
+
+//         // g)
+//         myVal = myVal+7
+
+//         // h)
+//         document.write("Value after addition is: "+myVal+"<br>")
+
+//         // i)
+//         myVal--
+
+//         // j)
+//         document.write("Value after decrement is:"+myVal+"<br>")
+
+//         // k)
+//         myVal=myVal%3
+
+//        // l)        
+//         document.write("The remainder is :"+myVal+"<br>")
+
+
+// // Q-4
+// var val1 = 600
+// var val2 = 5
+
+// document.write("Total cost to buy "+val2+" "+"Tickets is: "+ val1 * val2+" PKR "+"<br>")
+
+// // Q-5
+
+// var myInp = +prompt("Enter any Number:");
+
+// var myNum = 0;
+
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+
+// // Q-6          Done
+
+// // Q-7
+
+// var prc1 = +prompt("Price of item 1");
+// var prc2 = +prompt("Price of item 2");
+// var qty1 = +prompt("Ordered quantity of item 1");
+// var qty2 = +prompt("Ordered quantity of item 2");
+// var shpChg = +prompt("Shipping charges");
+
+// var TotCost;
+
+// TotCost = (prc1 * qty1) + (prc2 * qty2) + shpChg;
+
+
+// document.write("Shopping Cart"+"<br>")
+// document.write("Price of item 1: " + prc1 + "<br>");
+// document.write("Price of item 2: " + prc2 + "<br>");
+
+// document.write("Ordered quantity of item 1: " + qty1 + "<br>");
+// document.write("Ordered quantity of item 2: " + qty2 + "<br>");
+
+// document.write("Shipping charges: " + shpChg + "<br>");
+
+// document.write("Total cost of your order is: " + TotCost + "<br>");
+
+// // Q-8
+
+// var totMark = +prompt("Total Marks");
+// var markObtn = +prompt("Marks Obtained");
+
+// document.write("Marks Sheet"+"<br>");
+// document.write("Total Marks: " + totMark + "<br>");
+// document.write("Marks Obtained: " + markObtn + "<br>");
+// document.write("Percentage: " + (markObtn/totMark)*100 + "%" +"<br>");
+
+// // Q-9
+
+// document.write("Total Currency in PKR: "+ ((10 * 104.80) + (25 * 28)) + "<br>")
+
+// // Q-10
+
+// var val1 = +prompt("Enter any number");
+
+// valAdd = val1 + 5;
+// valMlt = val1 * 10;
+// valDiv = val1 / 2;
+
+// //var tot = valAdd + valMlt + valDiv;
+
+// document.write("Value After Addition: "+ valAdd +"<br>");
+// document.write("Value After Multiplication: "+ valMlt +"<br>");
+// document.write("Value After Division: "+ valDiv +"<br>");
+
+// document.write("Total: "+ (valAdd + valMlt + valDiv) +"<br>");
+
+// // Q-11
+
+// var CrntYr = +prompt("Current Year: ");
+// var BrthYr = +prompt("Birth Year: ");
+
+// document.write("Age Calculator "+"<br>");
+// document.write("Current Year: "+CrntYr+"<br>");
+// document.write("Current Year: "+BrthYr+"<br>");
+
+// document.write("Your Age is: "+(CrntYr - BrthYr)+"<br><br>");
+
+// // Q-12
+
+//     // a)
+//     var radius=+prompt("Enter Radius");
+
+//     // b)
+
+//     // Circumference of a circle = 2 π r
+//     //  π = 3.142
+//     document.write("The Geometrizer"+"<br>")
+//     document.writeln("Circumference of a circle: "+ (2 * 3.142 * radius)+"<br>");
+//     document.writeln("The area is: "+ (3.142 * (radius*radius))+"<br><br>");
+
+// // Q-13
+//     // a)
+//     var FavSnk = prompt("Your favorite snack");
+
+//     // b)
+//     var CrntAge = +prompt("Your current age");
+
+//     // c)
+//     var MaxAge = +prompt("Your maximum age");
+
+//     // d)
+//     var EstQty = +prompt("Estimated amount per day");    
+
+//     // e)
+
+//     var age = MaxAge - CrntAge;
+
+//     document.write("Your favorite snack: "+FavSnk+"<br>");
+//     document.write("Your current age: "+CrntAge+"<br>");
+//     document.write("Your maximum age: "+MaxAge+"<br>");
+//     document.write("Estimated amount per day: "+EstQty+"<br>");
+
+//     document.write("You will need "+(age*EstQty)+" "+FavSnk+" " +"to last you until the ripe old age of "+MaxAge+"<br>");
+
+// // Chapter No. 6  --- MATH EXPRESSIONS
+// // -----------------------------------
+
+// // Q - 1
+
+// var a = +prompt("Enter a Number:");
+
+// document.write("Result:"+"<br>");
+// document.write("The value of a is: "+ a +"<br>")
+// // document.write("---------------------"+"<br"+"<br>")
+
+// document.write("The value of ++a is: "+ ++a +"<br>")
+
+// document.write("Now the value of a is: "+ a +"<br>")
+
+// document.write("The value of a++ is: "+ a++ +"<br>")
+
+// document.write("Now the value of a is: "+ a +"<br>")
+
+// document.write("The value of --a is: "+ --a +"<br>")
+
+// document.write("Now the value of a is: "+ a +"<br>")
+
+// document.write("The value of a-- is: "+ a-- +"<br>")
+
+// document.write("Now the value of a is: "+ a +"<br>")
+
+// // Q - 2
+
+// var a = 2; 
+// var b = 1; 
+// var c;
+
+// document.write("a is: "+a+"<br>")
+// document.write("b is: "+b+"<br>")
+
+// c = --a;                        
+// c = -a - --b;                   
+// c = --a - --b + ++b;            
+// c = --a - --b + ++b + b--;      
+
+// document.write("Result is: "+c)
+
+// // Q-3
+
+// var UserNm = prompt("User Name:");
+// alert("Welcome Mr. "+UserNm+" "+"to my website")
+
+// // Q-4 is blank
+
+// // Q-5
+// var myInp = +prompt("Enter any Number:");
+
+// var myNum = 0;
+
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+// document.write(myInp+" * " + ++myNum+" = "+myInp * myNum +"<br>");
+
+// Q-6
+    // a)
+    var SbjNm1 = prompt("Enter Subject 1");
+    var SbjNm2 = prompt("Enter Subject 2");
+    var SbjNm3 = prompt("Enter Subject 3");
+
+    // b)
+    var TotMrk = 100;
+
+    // c)
+    var  SbjMrk1 = +prompt("Enter Marks 1");
+
+    // d)
+    var SbjMrk2 = +prompt("Enter Marks 2");
+    var SbjMrk3  = +prompt("Enter Marks 3");
+
+    // e)
+
+    document.write("Subject"+"          "+"Total Marks"+"          "+"Obtained Marks"+"          "+"Percentage"+"<br><br>");
+    document.write(SbjNm1+"               "+TotMrk+"               "+SbjMrk1+"               "+((SbjMrk1/TotMrk)*100)+"<br>");
+    document.write(SbjNm2+"               "+TotMrk+"               "+SbjMrk2+"               "+((SbjMrk2/TotMrk)*100)+"<br>");
+    document.write(SbjNm3+"               "+TotMrk+"               "+SbjMrk3+"               "+((SbjMrk3/TotMrk)*100)+"<br>");
+    document.write("                      "+(TotMrk*3)+"               "+(SbjMrk1+SbjMrk2+SbjMrk3)+"               "+(((((SbjMrk1/TotMrk)*100)+((SbjMrk2/TotMrk)*100)+((SbjMrk3/TotMrk)*100)))/3)+"<br>");
+
+
+//Q-7       Blank
